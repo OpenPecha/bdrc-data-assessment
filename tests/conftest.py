e9f084cc4420e5b9c4568867b9323d7fbf3d4bc2
tenkalden <tenkalden007@gmail.com>
2024-12-18 08:56:52 +0530

    minor change

--- tests/conftest.py
+++
@@ -1,8 +0,0 @@
-import os
-from dotenv import load_dotenv
-
-
-def pytest_configure(config):
-
-    dotenv_path = os.path.join(os.path.dirname(__file__), '..', '.test_env')
-    load_dotenv(dotenv_path)
